f88bd12c899ddeef0e53ec0335f4e9dac39f6794
41343150 <qqwoplay@gmail.com>
2025-12-31 18:54:17 +0800

    up

--- utils/bbox_utils.py
+++ utils/bbox_utils.py
@@ -6,13 +6,9 @@ def get_bbox_width(bbox):
     return bbox[2]-bbox[0]
 
 def measure_distance(p1,p2):
-    if p1 is None or p2 is None or len(p1) < 2 or len(p2) < 2:
-        return 0
     return ((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)**0.5
 
 def measure_xy_distance(p1,p2):
-    if p1 is None or p2 is None or len(p1) < 2 or len(p2) < 2:
-        return 0, 0
     return p1[0]-p2[0],p1[1]-p2[1]
 
 def get_foot_position(bbox):
